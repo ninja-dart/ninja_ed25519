f3bf833e772098ff6fa38d6f96520186af835aa0
Ravi Teja Gudapati <tejainece@gmail.com>
2021-08-12 10:39:19 +0200

    base64

--- lib/src/private_key.dart
+++ lib/src/private_key.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:ninja_ed25519/src/curve25519/curve25519.dart';
@@ -9,9 +10,26 @@ class PrivateKey {
   PrivateKey(this.bytes);
 
   factory PrivateKey.fromHex(String hex) {
+    if (hex.length == 128) {
+      hex = hex.substring(0, 64);
+    }
+    if (hex.length != 64) {
+      throw ArgumentError.value(hex, 'hex', 'invalid key length');
+    }
     final bytes = hex64ToBytes(hex);
     return PrivateKey(bytes);
   }
+  factory PrivateKey.fromBase64(String input) {
+    Uint8List bytes = base64Decode(input);
+    if (bytes.length == 64) {
+      bytes = bytes.sublist(0, 32);
+    }
+    if (bytes.length != 32) {
+      throw ArgumentError.value(input, 'input', 'invalid key length');
+    }
+    return PrivateKey(bytes);
+  }
+  // TODO fromBech32
 
   PublicKey get publicKey {
     final pubBytes = curve25519.scalarMultiplyBase(bytes).asBytes;
@@ -19,6 +37,8 @@ class PrivateKey {
   }
 
   String get asHex => bytesToHex(bytes);
+  String get asBase64 => base64Encode(bytes);
+  // TODO toBech32
 }
 
 class PublicKey {
@@ -26,5 +46,23 @@ class PublicKey {
 
   PublicKey(this.bytes);
 
+  factory PublicKey.fromHex(String hex) {
+    if (hex.length != 64) {
+      throw ArgumentError.value(hex, 'hex', 'invalid key length');
+    }
+    final bytes = hex64ToBytes(hex);
+    return PublicKey(bytes);
+  }
+  factory PublicKey.fromBase64(String input) {
+    Uint8List bytes = base64Decode(input);
+    if (bytes.length != 32) {
+      throw ArgumentError.value(input, 'input', 'invalid key length');
+    }
+    return PublicKey(bytes);
+  }
+  // TODO fromBech32
+
   String get asHex => bytesToHex(bytes);
+  String get asBase64 => base64Encode(bytes);
+  // TODO toBech32
 }
