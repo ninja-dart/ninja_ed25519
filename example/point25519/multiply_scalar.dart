ddc7dc068d97f75805797e15e9978360fc5d2fb9
Ravi Teja Gudapati <tejainece@gmail.com>
2021-08-24 08:04:54 +0200

    testing B multiplication

--- example/point25519/multiply_scalar.dart
+++ example/point25519/multiply_scalar.dart
@@ -3,13 +3,17 @@
 // R = 396fc23bc389046b214087a9522c0fbd673d2f3f00ab9768f35fa52f953fef22
 // a = fadf3558b700b88936113be1e5342245bd68a6b1deeb496000c4148ad4b61f02
 
+import 'dart:typed_data';
+
 import 'package:ninja/ninja.dart';
 import 'package:ninja_ed25519/curve.dart';
 import 'package:ninja_ed25519/src/curve25519/point.dart';
 
 void mul1() {
-  final RStr = '396fc23bc389046b214087a9522c0fbd673d2f3f00ab9768f35fa52f953fef22';
-  final aStr = 'fadf3558b700b88936113be1e5342245bd68a6b1deeb496000c4148ad4b61f02';
+  final RStr =
+      '396fc23bc389046b214087a9522c0fbd673d2f3f00ab9768f35fa52f953fef22';
+  final aStr =
+      'fadf3558b700b88936113be1e5342245bd68a6b1deeb496000c4148ad4b61f02';
   final R = Point25519.fromHex(RStr);
   final a = BigInt.parse(aStr, radix: 16);
   final D = R.multiplyScalar(a, Curve25519.order);
@@ -17,13 +21,23 @@ void mul1() {
 }
 
 void mul2() {
-  final RStr = '396fc23bc389046b214087a9522c0fbd673d2f3f00ab9768f35fa52f953fef22';
+  final RStr =
+      '396fc23bc389046b214087a9522c0fbd673d2f3f00ab9768f35fa52f953fef22';
   final R = Point25519.fromHex(RStr);
   final R2 = R.multiplyScalar(BigInt.two, Curve25519.order);
   print(R2.asBytes.toHex(outLen: 64));
 }
 
+// https://crypto.stackexchange.com/questions/27392/base-point-in-ed25519
+void mul3() {
+  final vUint =
+      Uint8List.fromList(BigInt.from(1).asBytes(outLen: 32).reversed.toList());
+  final v = curve25519.scalarMultiplyBase(vUint);
+  print(v.asBytes.reversed.asBigInt);
+}
+
 void main() {
   // mul1();
-  mul2();
+  // mul2();
+  mul3();
 }
